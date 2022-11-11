951c458bf27319a60985377f3f5ad677a548730b
Alex <alexesc154@gmail.com>
2022-11-11 22:36:25 +0300

    create metod changeCards and logic

--- blackjack_light/lib/screens/blackjack_screen.dart
+++ blackjack_light/lib/screens/blackjack_screen.dart
@@ -1,8 +1,9 @@
-import 'dart:ffi';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class BlackJackScreen extends StatefulWidget {
+  const BlackJackScreen({Key? key}) : super(key: key);
+
   @override
   State<BlackJackScreen> createState() => _BlackJackScreenState();
 }
@@ -10,18 +11,18 @@ class BlackJackScreen extends StatefulWidget {
 class _BlackJackScreenState extends State<BlackJackScreen> {
   bool isGameStarted = false;
 
-  List<Image> clientsCards = [];
-
-  String? clientsFirstCard;
-  String? clientsSecondCard;
+  List<Image> myCards = [];
 
   List<Image> dealersCards = [];
+
+  String? playersFirstCard;
+  String? playersSecondCard;
 
   String? dealersFirstCard;
   String? dealersSecondCard;
 
-  int clientsScore = 0;
-  int dealersScore = 0;
+  int playerScore = 0;
+  int dealerScore = 0;
 
   final Map<String, int> deckOfCards = {
     "cards/2.1.png": 2,
@@ -78,73 +79,195 @@ class _BlackJackScreenState extends State<BlackJackScreen> {
     "cards/A4.png": 11,
   };
 
-  Map<String, int> playCards = {};
+  Map<String, int> playingCards = {};
 
   @override
   void initState() {
-    playCards.addAll(deckOfCards);
+    // TODO: implement initState
     super.initState();
+    playingCards.addAll(deckOfCards);
   }
 
   void changeCards() {
     setState(() {
       isGameStarted = true;
     });
+
+    playingCards = {};
+
+    playingCards.addAll(deckOfCards);
+
+    myCards = [];
+    dealersCards = [];
+
+    Random random = Random();
+
+    String cardOneKey = playingCards.keys.elementAt(random.nextInt(
+        playingCards.length)); // from 1 to playingCards.length inclusive
+
+    //Making sure cards are unique and remove cardOneKey from playingCards
+    playingCards.removeWhere((key, value) => key == cardOneKey);
+
+    String cardTwoKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardTwoKey);
+
+    String cardThreeKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardThreeKey);
+
+    String cardFourKey =
+        playingCards.keys.elementAt(random.nextInt(playingCards.length));
+    playingCards.removeWhere((key, value) => key == cardFourKey);
+
+    dealersFirstCard = cardOneKey;
+    dealersSecondCard = cardTwoKey;
+
+    playersFirstCard = cardThreeKey;
+    playersSecondCard = cardFourKey;
+
+    dealersCards.add(Image.asset(dealersFirstCard!));
+
+    dealersCards.add(Image.asset(dealersSecondCard!));
+
+    dealerScore =
+        deckOfCards[dealersFirstCard]! + deckOfCards[dealersSecondCard]!;
+
+    myCards.add(Image.asset(playersFirstCard!));
+    myCards.add(Image.asset(playersSecondCard!));
+
+    playerScore =
+        deckOfCards[playersFirstCard]! + deckOfCards[playersSecondCard]!;
+
+    if (dealerScore <= 14) {
+      String thirdDealersCardKey =
+          playingCards.keys.elementAt(random.nextInt(playingCards.length));
+      playingCards.removeWhere((key, value) => key == thirdDealersCardKey);
+
+      dealersCards.add(Image.asset(thirdDealersCardKey));
+
+      dealerScore = dealerScore + deckOfCards[thirdDealersCardKey]!;
+    }
   }
 
-  void addCard() {}
+  void addCard() {
+    Random random = Random();
+
+    if (playingCards.length > 0) {
+      String cardKey =
+          playingCards.keys.elementAt(random.nextInt(playingCards.length));
+      playingCards.removeWhere((key, value) => key == cardKey);
+
+      setState(() {
+        myCards.add(
+          Image.asset(cardKey),
+        );
+      });
+
+      playerScore = playerScore + deckOfCards[cardKey]!;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: isGameStarted == true
-            ? SafeArea(
-                child: Center(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: isGameStarted == true
+          ? SafeArea(
+              child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // dealers Card
+                  Column(
+                    children: [
+                      Text(
+                        "Dealer Score $dealerScore",
+                        style: TextStyle(
+                          color: dealerScore <= 21
+                              ? Colors.green[900]
+                              : Colors.red[900],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      // Grid View
+                      CardsGridView(cards: dealersCards),
+                    ],
+                  ),
+                  // players Card
+                  Column(
+                    children: [
+                      Text(
+                        "Player Score $playerScore",
+                        style: TextStyle(
+                          color: playerScore <= 21
+                              ? Colors.green[900]
+                              : Colors.red[900],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      // Grid View
+                      CardsGridView(cards: myCards),
+                    ],
+                  ),
+                  // 2 buttons
+                  IntrinsicWidth(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        //dealers cards
-                        Column(
-                          children: [
-                            Text("Dealer score $dealersScore"),
-                            SizedBox(height: 20),
-                          ],
-                        ),
-                        //playeer cards
-                        Column(
-                          children: [
-                            Text("Playeer score $clientsScore"),
-                            SizedBox(height: 20),
-                          ],
-                        ),
-                        // 2 button
-                        IntrinsicWidth(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              MaterialButton(
-                                child: Text("Another Card"),
-                                color: Colors.brown[200],
-                                onPressed: addCard,
-                              ),
-                              MaterialButton(
-                                child: Text("Next Round"),
-                                color: Colors.brown[200],
-                                onPressed: addCard,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ]),
-                ),
-              )
-            : Center(
-                child: MaterialButton(
-                    onPressed: () {
-                      changeCards();
-                    },
-                    child: Text("Start")),
-              ));
+                        MyButton(onPressed: addCard, label: "Another Card"),
+                        MyButton(onPressed: changeCards, label: "Next Round"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ))
+          : Center(
+              child: MyButton(onPressed: changeCards, label: "Start Game"),
+            ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  MyButton({required this.onPressed, required this.label});
+
+  final Function() onPressed;
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.brown[200],
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
+
+class CardsGridView extends StatelessWidget {
+  const CardsGridView({
+    required this.cards,
+  });
+
+  final List<Image> cards;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: cards.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: cards[index],
+            );
+          }),
+    );
   }
 }

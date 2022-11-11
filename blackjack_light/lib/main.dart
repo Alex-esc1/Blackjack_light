a6b0f0157ea9fa8745567a326b91bfb54889c689
Alex <alexesc154@gmail.com>
2022-11-11 21:12:18 +0300

    build screen

--- blackjack_light/lib/main.dart
+++ blackjack_light/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blackjack_light/screens/blackjack_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home: BlackJackScreen(),
     );
   }
 }
-
---
+++ blackjack_light/lib/screens/blackjack_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BlackJackScreen extends StatefulWidget {
+  @override
+  State<BlackJackScreen> createState() => _BlackJackScreenState();
+}
+
+class _BlackJackScreenState extends State<BlackJackScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
